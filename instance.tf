f5918f0e68f8b5150d94e4d739231d05fb6e208c
Pierrick MOLERA <pierrick@pop-os.localdomain>
2020-05-20 11:11:03 +0200

    #1 change machine type

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "lab" {
   count = 2   
-  ami           = "ami-0c229bfed6d47178b"
-  instance_type = "t2.micro"
+  ami           = "ami-0f3930953baf5a5bf"
+  instance_type = "t2.xlarge"
 
   # the VPC subnet
   subnet_id = aws_subnet.lab-public-1.id
@@ -13,7 +13,7 @@ resource "aws_instance" "lab" {
   key_name = aws_key_pair.mykeypair.key_name
 
   tags = {
-    Name  = "${var.machines_lab[count.index].name}" #"lab-${count.index}"
+    Name  = "${var.machines_lab[count.index].name}"
   }
 
   connection {
@@ -35,7 +35,10 @@ resource "aws_instance" "lab" {
         "sudo chmod 700 /home/${var.machines_lab[count.index].name}/.ssh",
         "sudo chmod 600 /home/${var.machines_lab[count.index].name}/.ssh/authorized_keys",
         "sudo usermod -aG sudo ${var.machines_lab[count.index].name}",
-        "sudo passwd -d ${var.machines_lab[count.index].name}"
+        "sudo passwd -d ${var.machines_lab[count.index].name}",
+        "sudo apt-get update",
+        "sudo dpkg --configure -a",
+        "sudo apt -y install virtualbox"
    ]
  }
 
